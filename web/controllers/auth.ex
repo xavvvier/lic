c57be250c7b939e1487c9134f57bccb6b45283a8
alternar64@gmail.com <xavvvier@users.noreply.github.com>
2016-05-27 14:12:37 -0500

    Fixed authentication, added query result page

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -9,8 +9,19 @@ defmodule Lic.Auth do
 
   def call(conn, _repo) do
     user_id = get_session(conn, :user_id)
-    user = user_id && get_by_id(user_id)
-    assign(conn, :current_user, user)
+    cond do
+      user = conn.assigns[:current_user] ->
+        conn
+      user = user_id && get_by_id(user_id) ->
+        IO.inspect user
+        assign(conn, :current_user, user)
+      true ->
+        assign(conn, :current_user, nil)
+    end
+    # IO.inspect conn.assigns[:current_user]
+    # conn
+    # user = user_id && get_by_id(user_id)
+    # assign(conn, :current_user, user)
   end
 
   def authenticate(conn, _opts) do
@@ -27,11 +38,10 @@ defmodule Lic.Auth do
   def login(conn, user, pass) do
     user = get_user(user, pass)
     if user do
-        conn
+        conn = conn
         |> assign(:current_user, user)
-        |> put_session(:user_id, user)
+        |> put_session(:user_id, user.id)
         |> configure_session(renew: true)
-        IO.inspect conn.assigns
         {:ok, conn}
     else
         {:error, :unauthorized, conn}
@@ -42,25 +52,25 @@ defmodule Lic.Auth do
     configure_session(conn, drop: true)
   end
 
-  defp get_user(user, pass) do
+  def get_user(user, pass) do
     valid_user = [
       Application.get_env(:lic, Lic.Auth)[:user],
       Application.get_env(:lic, Lic.Auth)[:pass]
     ]
     case valid_user  do
       [^user, ^pass] ->
-        %{user: user}
+        %{username: user, id: user}
       _ ->
         nil
     end
   end
 
-  defp get_by_id(user_id) do
+  def get_by_id(user_id) do
     valid_user = Application.get_env(:lic, Lic.Auth)[:user]
     case user_id do
-      valid_user ->
-        %{user: user_id}
-      true ->
+      ^valid_user ->
+        %{username: user_id, id: user_id}
+      _ ->
         nil
     end
   end
--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -1,7 +1,13 @@
 defmodule Lic.SearchController do
   use Lic.Web, :controller
+  alias Lic.Repo
+  alias Lic.License
 
   def create(conn, %{"search" => %{"number" => data}}) do
-    render conn, "view.html", result: "Hey: #{data}"
+    licenses = Repo.all from l in License,
+      join: p in assoc(l, :product),
+      where: p.serial_number == ^data,
+      select: [l.number, l.description]
+    render conn, "view.html", result: licenses
   end
 end
